29aec7ce5939bdc84b27df9176972527df11bb71
Codemzy <codemzy@gmail.com>
2020-10-21 13:27:17 +0000

    Completed exercise 2 styles

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -30,15 +30,37 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 //   background: '#f1f2f7',
 //   color: '#434449',
 
+const Button = styled.button(props => {
+  return {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+    color: props.variant === "primary" ? 'white' : props.variant === "secondary" ? '#434449' : '',
+    background: props.variant === "primary" ? '#3f51b5' : props.variant === "secondary" ? '#f1f2f7' : ''
+  }
+});
+
 // Input
 //   borderRadius: '3px',
 //   border: '1px solid #f1f1f4',
 //   background: '#f1f2f7',
 //   padding: '8px 12px',
+const Input = styled.input({
+  borderRadius: '3px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+});
+
 
 // FormGroup
 //   display: 'flex',
 //   flexDirection: 'column',
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+});
 
 // 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
@@ -68,4 +90,4 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-export {CircleButton, Dialog}
+export {CircleButton, Dialog, FormGroup, Input, Button}
