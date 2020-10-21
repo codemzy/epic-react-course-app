0c760a2ce0c5282815a80a3d1303b10d99346aab
Codemzy <codemzy@gmail.com>
2020-10-21 11:16:46 +0000

    Completed render exercise in bootstrap - extra 1

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,32 +1,39 @@
-// 🐨 you'll need to import React and ReactDOM up here
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+// reach dialog
+import {Dialog} from "@reach/dialog";
+import '@reach/dialog/styles.css'
+// components
+import {Logo} from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
-import {Logo} from './components/logo';
-
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
 function App() {
+    // state
+    const [openModal, setOpenModal] = React.useState("none");
 
-    function clickLogin(event) {
-        alert("The login button was clicked");
-    };
-
-    function clickRegister(event) {
-        alert("The register button was clicked");
-    };
+    // modal controls
+    const open = (type) => setOpenModal(type);
+    const close = () => setOpenModal("none");
 
     return (
         <div>
             <Logo width="80" height="80" />
             <h1>Bookshelf</h1>
-            <button onClick={clickLogin}>Login</button>
-            <button onClick={clickRegister}>Register</button>
+            <div>
+                <button onClick={() => open('login')}>Login</button>
+            </div>
+            <div>
+                <button onClick={() => open('register')}>Register</button>
+            </div>
+            <Dialog aria-label="Login form" isOpen={openModal === "login"} onDismiss={close}>
+                <p>This is the log in modal</p>
+                <button onClick={close}>Okay</button>
+            </Dialog>
+            <Dialog aria-label="Registration form" isOpen={openModal === "register"} onDismiss={close}>
+                <p>This is the register modal</p>
+                <button onClick={close}>Okay</button>
+            </Dialog>
         </div>
-    );
-};
+    )
+}
 
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
