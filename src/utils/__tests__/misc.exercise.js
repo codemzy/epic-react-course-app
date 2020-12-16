408892ca71e76af73ee41b00d7f43f80108cd692
Codemzy <codemzy@gmail.com>
2020-12-16 11:50:00 +0000

    Added test for formatDate

--- src/utils/__tests__/misc.exercise.js
+++ src/utils/__tests__/misc.exercise.js
@@ -1,2 +1,7 @@
-test.todo('formatDate formats the date to look nice')
+import {formatDate} from '../misc'
+
+// exercise - test formatDate function
+test('formatDate formats the date to look nice', () => {
+    expect(formatDate(new Date('December 16, 2020'))).toBe('Dec 20');
+});
 
